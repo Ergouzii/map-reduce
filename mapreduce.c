00a93edd399abd529b3084eba820bc194a7464bf
Ergouzii <jayliu925@gmail.com>
2019-10-23 11:28:56 -0600

    MR_Emit early done

--- mapreduce.c
+++ mapreduce.c
@@ -4,14 +4,42 @@
 #include <unistd.h>
 #include <string.h>
 #include <assert.h>
+#include <pthread.h>
 
 #include "mapreduce.h"
 #include "threadpool.h"
+
+typedef struct K_V_Pair {
+    char *key;
+    char *value;
+    struct K_V_Pair *next;
+} K_V_Pair;
+
+typedef struct {
+    K_V_Pair *head;
+    pthread_mutex_t mutex;
+    unsigned long partition_num;
+} Pair_Table;
+
+int NUM_PARTITIONS;
+Pair_Table *PAIR_TABLES;
 
 void MR_Run(int num_files, char *filenames[], 
             Mapper map, int num_mappers,
             Reducer concate, int num_reducers) {
 
+    NUM_PARTITIONS = num_reducers;
+    
+    // initialize each element in PAIR_TABLES
+    PAIR_TABLES = (Pair_Table *)(malloc(NUM_PARTITIONS * sizeof(Pair_Table))); //TODO: free it!
+    for (int i = 1; i <= NUM_PARTITIONS; i++) {
+        Pair_Table new_table;
+        PAIR_TABLES[i] = new_table;
+        PAIR_TABLES[i].head = NULL;
+        pthread_mutex_init(&(PAIR_TABLES[i].mutex), NULL);
+        PAIR_TABLES[i].partition_num = i;
+    }
+    
     ThreadPool_t *tp = ThreadPool_create(num_mappers); // create a threadpool for mappers
     
     // add each file_name to work queue
@@ -27,8 +55,35 @@ MR_Emit takes a key and a value associated with it, and writes this pair to a
 specific partition which is determined by passing the key to MR_Partition 
 */
 void MR_Emit(char *key, char *value) {
-    char *key;
-    
+    K_V_Pair *new_pair;
+    new_pair -> key = key;
+    new_pair -> value = value;
+
+    unsigned long partition_num = MR_Partition(key, NUM_PARTITIONS);
+    Pair_Table pair_table;
+    for (int i = 0; i < NUM_PARTITIONS; i++) {
+        pair_table = PAIR_TABLES[i];
+        pthread_mutex_lock(&(pair_table.mutex));
+        // insert new_pair to matching partition
+        if (pair_table.partition_num == partition_num) { 
+            K_V_Pair *cur = pair_table.head;
+            // insertion sort
+            if (cur == NULL) {
+                cur = new_pair;
+            } else if (strcmp(cur -> key, new_pair -> key) <= 0) {
+                new_pair -> next = cur;
+                cur = new_pair;
+            } else {
+                while ((cur -> next != NULL) && 
+                        (strcmp(new_pair -> key, cur -> next -> key) > 0)) {
+                    cur = cur -> next;
+                }
+                new_pair -> next = cur -> next;
+                cur -> next = new_pair;
+            }
+        }
+        pthread_mutex_unlock(&(pair_table.mutex));
+    }
 }
 
 // source: assignment 2 instructions
