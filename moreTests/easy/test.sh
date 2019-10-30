a5535801b4b198b3b7e4583399548cc3d89d7e39
Xiaohui Liu <xiaohui1@cs.ualberta.ca>
2019-10-29 19:34:08 -0600

    clean everything when done testing

--- moreTests/easy/test.sh
+++ moreTests/easy/test.sh
@@ -38,3 +38,7 @@ make clean-result
 ./validation 5 result-0.txt result-1.txt result-2.txt result-3.txt result-4.txt 2-correct_result-0.txt 2-correct_result-1.txt 2-correct_result-2.txt 2-correct_result-3.txt 2-correct_result-4.txt
 
 make clean-all
+
+cd ..
+
+make clean-all
